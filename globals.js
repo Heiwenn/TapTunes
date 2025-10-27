39e8e5716aeba4244388e166797dd73cb53eb3da
Lucas Polanco <lucaspolanc0@vt.edu>
2025-10-27 01:12:14 -0400

    fix

--- globals.js
+++ globals.js
@@ -6,8 +6,8 @@
 
 
 var aspectRatio = 3 / 4; //Typical Monitor Aspect Ratio
-var menuSoundTrack = "/assets/music/vlog-beat-background-349853.mp3";
-var backgroundImage = "/assets/images/beautiful-7305567_1280.jpg";
+var menuSoundTrack = "assets/music/vlog-beat-background-349853.mp3";
+var backgroundImage = "assets/images/beautiful-7305567_1280.jpg";
 
 var bg;
 var menuMusic;
